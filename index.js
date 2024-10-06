316ca75ddd3b2faff73a8a27dbe8c17f96e2c64b
chris <Mileschris466@gmail.com>
2024-10-06 10:33:00 +0300

    event listener

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+
+    let input = document.getElementById("button");
+    input.addEventListener("click", function() {
+        console.log("Button clicked");
+    });
 }
+
+
